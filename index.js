081af2add0dbcc14edeacf7302b7c079ac3268c8
Finn <derfinn@gmail.com>
2014-08-14 01:09:14 +0200

    working, but not really

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var pg = require('pg')
-var QueryStream = require('pg-query-stream')
-// var copyFrom = require('pg-copy-streams').from
+var pg = require('pg.js')
+var copy = require('pg-copy-streams')
+var fs = require('fs')
+var crypto = require('crypto')
 
 module.exports = Blobstore
 
@@ -11,37 +12,42 @@ function Blobstore(opts) {
   this.table = opts.table || 'blob'
 }
 
-Blobstore.prototype.createReadStream = function (opts) {
-  // is there a cooler way to do this?
-  if(!opts.hash) throw new Error('You have to specify a hash')
-  var stream = new Transform
-  stream._transform = function (a, enc, cb) { this.push(a); cb() }
-  pg.connect(this.url, function (err, client, done) {
-    var query = new QueryStream('SELECT $1 FROM $2', [opts.hash, this.table])
-    client.query(query).pipe(stream).on('end', function () {
-      done()
-    })
+Blobstore.prototype.createReadStream = function createReadStream(opts) {
+  if(!opts.hash) throw new Error('You have to specify a hash key')
+  var client = new pg.Client(this.url)
+  client.connect()
+  var query = copy.to('COPY (SELECT value FROM blob.blob WHERE key=\'' + opts.hash +'\') TO STDOUT (FORMAT text)')
+  var stream = client.query(query)
+  stream.on('end', function () {
+    client.end()
+  })
+  stream.on('error', function (err) {
+    console.error(err)
+    client.end()
   })
   return stream
 }
 
-//https://github.com/brianc/node-postgres/wiki/Client different way without callback?
-Blobstore.prototype.createWriteStream = function (cb) {
-  throw new Error('no implemented')
-  var stream = new Transform
-  stream._transform = function (a, enc, cb) { this.push(a); cb() }
-  pg.connect(this.url, function (err, client, done) {
-    var toDb = client.copyFrom('COPY keys (value, key) WITH BINARY FROM STDIN WITH CSV')
-    toDb.on('close', function () {
-      done()
-      db({hash: 'TBD'})
-    })
-    stream.pipe(toDb).on('end', function () {
-      toDb.write('\t')
-    })
+Blobstore.prototype.createWriteStream = function createWriteStream(cb) {
+  var client = new pg.Client(this.url)
+  client.connect()
+  var query = copy.from('COPY blob.blob (key, value) FROM STDIN')
+  var stream = client.query(query)
+  var randomKey = crypto.randomBytes(32).toString('hex')
+  stream.write(randomKey)
+  stream.write('\t')
+  stream.on('end', function () {
+    cb({hash: randomKey}) // note this is random right now!
+    client.end()
   })
   return stream
 }
+//example
+// var test = createWriteStream(function (data) {
+//   console.log(data.key)
+// })
+// test.write('myvalues')
+// test.end()
 
 Blobstore.prototype.exists = function (metadata, cb) {
   // pg.connect(this.url, function (err, client, done) {
--- testing.js
+++ testing.js
@@ -1,19 +1,39 @@
-var pg = require('pg.js')
-var copy = require('pg-copy-streams')
-var fs = require('fs')
-var crypto = require('crypto')
-
+var blob = require('./')
 var url = 'postgresql://localhost:5432/blob'
+var store = new blob({url: url})
 
-// SELECT DATA
+var fs = require('fs')
+var file = fs.createReadStream('./example.js', {encoding: 'base64'})
 
-// pg.connect(url, function (err, client, done) {
-//   var hash = "thekey"
-//   var table = "blob.blob"
-//   if(err) throw err
-//   var query = copy.to('COPY (SELECT value FROM blob.blob WHERE key=\'' + hash +'\') TO STDOUT (FORMAT text)')
+var save = store.createWriteStream(function (data) {
+  console.log('saved ' + data.hash)
+  var readStream = store.createReadStream(data)
+  readStream.pipe(fs.createWriteStream('./blablub.js'))
+})
+
+file.pipe(save)
+
+// save.write('sdfsdfdf')
+// save.end()
+
+// var pg = require('pg.js')
+// var copy = require('pg-copy-streams')
+// var fs = require('fs')
+// var crypto = require('crypto')
+// 
+// var url = 'postgresql://localhost:5432/blob'
+// 
+// // SELECT DATA
+// 
+// 
+// 
+// 
+// function createReadStream(opts) {
+//   if(!opts.hash) throw new Error('You have to specify a hash key')
+//   var client = new pg.Client(url)
+//   client.connect()
+//   var query = copy.to('COPY (SELECT value FROM blob.blob WHERE key=\'' + opts.hash +'\') TO STDOUT (FORMAT text)')
 //   var stream = client.query(query)
-//   stream.pipe(process.stdout)
 //   stream.on('end', function () {
 //     client.end()
 //   })
@@ -21,34 +41,30 @@ var url = 'postgresql://localhost:5432/blob'
 //     console.error(err)
 //     client.end()
 //   })
+//   return stream
+// }
+// 
+// function createWriteStream(cb) {
+//   var client = new pg.Client(url)
+//   client.connect()
+//   var query = copy.from('COPY blob.blob (key, value) FROM STDIN')
+//   var stream = client.query(query)
+//   var randomKey = crypto.randomBytes(32).toString('hex')
+//   stream.write(randomKey)
+//   stream.write('\t')
+//   stream.on('end', function () {
+//     cb({hash: randomKey}) // note this is random right now!
+//     client.end()
+//   })
+//   return stream
+// }
+// 
+// var test = createWriteStream(function (data) {
+//   // console.log(data.hash)
+//   createReadStream(data).pipe(process.stdout)
 // })
-
-
-// INSERT DATA
-
-function createWriteStream(cb) {
-  var client = new pg.Client(url)
-  client.connect()
-  var query = copy.from('COPY blob.blob (key, value) FROM STDIN')
-  var stream = client.query(query)
-  var randomKey = crypto.randomBytes(32).toString('hex')
-  stream.write(randomKey)
-  stream.write('\t')
-  stream.on('end', function () {
-    console.log('over')
-    cb({key: randomKey})
-    client.end()
-  })
-  return stream
-}
-
-var test = createWriteStream(function (data) {
-  console.log(data.key)
-})
-test.write('myvalues')
-test.end()
-
-
+// test.write(new Buffer('myvalues'))
+// test.end()
 
 // pg.connect(url, function (err, client, done) {
 //   var stream = copy.from('COPY blob.blob FROM STDIN')
